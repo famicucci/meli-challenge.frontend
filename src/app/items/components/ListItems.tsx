39f6beb3578f9aa7cee618f4bd7284e0dfdd4653
Francisco Micucci <fmicucci@piconsulting.com.ar>
2023-09-26 15:19:37 -0300

    Show only 4 products in search

--- src/app/items/components/ListItems.tsx
+++ src/app/items/components/ListItems.tsx
@@ -6,10 +6,11 @@ import Item from "./Item";
 export default async function ListItems(props: ListItemsProps) {
   const { textSearch } = props;
   const data = await getItems(textSearch);
+  const numberOfItems = 4;
 
   return (
     <div>
-      {data.items.map((item: ItemProps) => (
+      {data.items.slice(0, numberOfItems).map((item: ItemProps) => (
         <Item key={item.id} item={item} />
       ))}
     </div>
