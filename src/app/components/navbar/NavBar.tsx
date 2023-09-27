ded44df4502af9bdb46c77ce497afb974d30d678
Francisco Micucci <fmicucci@piconsulting.com.ar>
2023-09-27 12:55:37 -0300

    Add logo and styles to the navbar

--- src/app/components/navbar/NavBar.tsx
+++ src/app/components/navbar/NavBar.tsx
@@ -1,10 +1,15 @@
 import styles from "../../styles/menu.module.css";
 import { FormSearch } from "../search";
+import Image from "next/image";
+import logo from "../../../../public/logo-meli.png";
 
 export default function NavBar() {
   return (
     <nav className={styles.nav}>
-      <FormSearch />
+      <div className={styles.logoAndSearch}>
+        <Image src={logo} alt="logo-mercadolibre" width={40} />
+        <FormSearch />
+      </div>
     </nav>
   );
 }
--- src/app/components/search/FormSearch.tsx
+++ src/app/components/search/FormSearch.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import styles from "@/app/styles/formSearch.module.css";
+import { Search } from "react-feather";
 
 export default function FormSearch() {
   const [value, setValue] = useState("");
@@ -18,7 +20,7 @@ export default function FormSearch() {
   };
 
   return (
-    <form onSubmit={onSubmit}>
+    <form onSubmit={onSubmit} className={styles.form}>
       <input
         type="text"
         placeholder="Nunca dejes de buscar"
@@ -26,8 +28,11 @@ export default function FormSearch() {
         onChange={(e) => {
           setValue(e.target.value);
         }}
+        className={styles.input}
       />
-      <button>Search</button>
+      <button className={styles.button}>
+        <Search size={16} />
+      </button>
     </form>
   );
 }
